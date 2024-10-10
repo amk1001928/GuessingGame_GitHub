6e1f806901ce16cd9d76c5448cbd57851319fdad
Your Name <your.email@example.com>
2024-10-10 15:52:38 +0300

    All task done

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -4,21 +4,30 @@ public class GuessingGame {
     public static void main(String[] args) throws Exception {
 
     Scanner scanner = new Scanner (System.in);
-
+    
     String correctName = "Emma";
     String guesses = "";
+    int guessesCount = 0;
 
 
+    do {
         System.out.println("Guess my name (type stop to exit)");
         guesses = scanner.nextLine();
-
+        
 
         if (guesses.equalsIgnoreCase("Stop")) {
+            break;
         }
+        guessesCount++;
 
-        if (guesses.equalsIgnoreCase("Emma")) {
-        System.out.println("Congratulations!");
-    }
+        if (guesses.equalsIgnoreCase(correctName)) {
+            System.out.println("Congratulations!");
+            break;
+        }
+        }
+        while (true);
+        System.out.println("You guessed " + guessesCount + " times.");
 
+        
     }
 }
