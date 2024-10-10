89944b51904c039b0349545c59ec89dd70df4819
Your Name <your.email@example.com>
2024-10-10 15:17:57 +0300

    Print Guessing name

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class GuessingGame {
     public static void main(String[] args) throws Exception {
+
+    Scanner scanner = new Scanner (System.in);
+
+    String correctName = "Emma";
+    String guesses = "";
+
+        System.out.println("Guess my name (type stop to exit)");
+        guesses = scanner.nextLine();
 
     }
 }
